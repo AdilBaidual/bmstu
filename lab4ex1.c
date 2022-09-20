b88cc13b75c641c96dbccfeaa0bd4665c9762f65
AdilBaidual <baidual.adil@yandex.ru>
2022-09-20 23:10:22 +0300

    lab4 -check

--- lab4ex1.c
+++ lab4ex1.c
@@ -6,14 +6,18 @@
 
 void Input(double *a);
 void ArraySize(double *n);
-void Array(double *arr, double n);
+double Array(double *arr, double n);
+int FindMax(double *arr, double n);
+int FindMin(double *arr, double n);
+double Func(double mn, double mx);
+void Var0(double *arr, double n);
 
 void main() {
-    double *arr;
+    double arr;
     double n;
     ArraySize(&n);
-    Array(arr, n);
-    printf("%lf", arr);
+    Array(&arr, n);
+    Var0(&arr, n);
     getch();
 }
 
@@ -26,14 +30,51 @@ void ArraySize(double *n) {
     Input(n);
 }
 
-void Array(double *arr, double n) {
-    arr = malloc(n * sizeof(double));
+double Array(double *arr, double n) {
+    double *mas = malloc(n * sizeof(double));
     for(int i = 0; i < n; i++) {
         double x;
         Input(&x);
         *(arr + i) = x;
     }
+    arr = mas;
+}
+
+int FindMax(double *arr, double n) {
+    int mx = 0;
     for(int i = 0; i < n; i++) {
-        printf("%lf\n", *(arr + i));
+        if(*(arr + i) > *(arr + mx)) {
+            mx = i;
+        }
     }
+    //printf("%d\n", mx);
+    return mx;
+}
+
+int FindMin(double *arr, double n) {
+    int mn = 0;
+    for(int i = 0; i < n; i++) {
+        if(*(arr + i) < *(arr + mn)) {
+            mn = i;
+        }
+    }
+    printf("%d\n", mn);
+    return mn;
+}
+
+double Func(double mn, double mx) {
+    double f = (mx + mn) / 2;
+    return f;
+}
+
+void Var0(double *arr, double n) {
+    double sum = 0;
+    int start = FindMax(arr, n);
+    double f = Func(FindMin(arr, n), FindMax(arr, n));
+    for(int i = start + 1; i < n; i ++) {
+        if(*(arr + i) > f) {
+            sum += *(arr + i);
+        }
+    }
+    printf("%lf", sum);
 }
